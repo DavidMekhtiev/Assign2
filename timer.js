c3a1cd631b3079cef6693de2124321bae2268495
DavidMekhtiev <mekhtiev.1995@bk.ru>
2023-11-22 19:56:54 +0600

    modified:   cart.html 	modified:   game.html 	modified:   index.html 	new file:   quiz.html 	modified:   registration.html 	modified:   style.css 	modified:   timer.js 	modified:   todo.html

--- timer.js
+++ timer.js
@@ -2,6 +2,10 @@ const durationInput = document.getElementById('duration');
 const startButton = document.getElementById('startBtn');
 const timerDisplay = document.getElementById('timer');
 
+const searchInput = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
+
+
 let countdown;
 
 startTimer(3600);
@@ -32,3 +36,65 @@ function displayTimeLeft(seconds) {
     const display = `Скидка закончится: ${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
     timerDisplay.textContent = display;
 }
+
+const timing = {
+    duration: 500,
+    iterations: 1,
+  };
+
+  const widthing = [
+    { width: '200px' },
+    { width: '300px' },
+  ];
+
+  const widthing2 = [
+    { width: '300px' },
+    { width: '200px' },
+  ];
+
+searchInput.addEventListener('focus', () => {
+    searchInput.animate(widthing,timing);
+    searchInput.style.width = '300px';
+  });
+  
+  searchInput.addEventListener('blur', () => {
+    searchInput.animate(widthing2,timing);
+     searchInput.style.width = '200px';
+  });
+
+  
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById('openModalBtn');
+  var span = document.getElementById('closeModalBtn');
+
+  btn.addEventListener('click', function () {
+      modal.style.display = 'block';
+  });
+  span.addEventListener('click', function () {
+      modal.style.display = 'none';
+  });
+  window.addEventListener('click', function (event) {
+      if (event.target === modal) {
+          modal.style.display = 'none';
+      }
+  });
+
+
+
+  window.onscroll = function() {
+    showScrollToTopButton();
+  };
+
+  function showScrollToTopButton() {
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+  }
+
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
